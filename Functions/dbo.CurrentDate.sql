fb05e7443892faaff29139758dd0595f4b62cee4
BartmanDilaw <fraternite76-alain@yahoo.fr>
2016-06-11 13:17:55 +0200

    Mise à jour de la fonction de date = à ladate courante.

--- Functions/dbo.CurrentDate.sql
+++ Functions/dbo.CurrentDate.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -6,7 +7,7 @@ CREATE FUNCTION [dbo].[CurrentDate] (
 )
 RETURNS datetime
 BEGIN
-  RETURN '12/06/2016 20:00:00' /* Change to real argument */
-  -- RETURN getdate() /* Change to real argument */
+  -- RETURN '12/06/2016 20:00:00' /* Change to real argument */
+  RETURN getdate() /* Change to real argument */
 END
 GO
